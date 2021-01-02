e64cdfa82987518ceba6930fe65c8b558f85d883
Mike <mi5hqaaaaov@gmail.com>
2021-01-02 17:47:27 +0200

    v1.1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 /*===== MENU SHOW =====*/
 const showMenu = (toggleId,navId) => {
     const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+        nav = document.getElementById(navId);
 
     if (toggle && nav) {
         toggle.addEventListener('click',() => {
-            nav.classList.toggle('show')
-        })
+            nav.classList.toggle('show');
+        });
     }
-}
-showMenu('nav-toggle','nav-menu')
+};
+showMenu('nav-toggle','nav-menu');
 
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');
@@ -20,28 +20,29 @@ function linkAction() {
     this.classList.add('active');
 
     /*Remove menu mobile*/
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
 }
 navLink.forEach(n => n.addEventListener('click',linkAction));
 
 /*===== tab menu =====*/
 
-const menuList = document.querySelectorAll('.list-item'),
+const menuList = document.querySelectorAll('.skills__list'),
     trigger = document.querySelectorAll('.skills__data');
 
 
-trigger.forEach((item) => {
-    item.addEventListener('click',(e) => {
-        e.target
-        menuList.forEach(item => {
+trigger.forEach((item,i) => {
+    item.addEventListener('click',() => {
 
+        menuList.forEach((item) => {
+            trigger[i].nextElementSibling.classList.toggle('show');
             item.classList.toggle('hide');
+
             item.classList.add('fade');
 
-        })
+        });
     });
-})
+});
 
 
 
@@ -66,8 +67,8 @@ sr.reveal('.home__social-icon',{ interval: 200 });
 sr.reveal('.about__text',{ delay: 400 });
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{});
-sr.reveal('.skills__text',{});
+// sr.reveal('.skills__subtitle',{});
+// sr.reveal('.skills__text',{});
 // sr.reveal('.skills__data',{ interval: 200 });
 sr.reveal('.skills__img',{ delay: 600 });
 // sr.reveal('.skills__list',{ delay: 600 });
