2ecdb0988bb0ff76305070a2c2cb6ac823a38a4e
Mike <mi5hqaaaaov@gmail.com>
2021-01-03 19:12:28 +0200

    v1.2

--- js/main.js
+++ js/main.js
@@ -58,17 +58,18 @@ const sr = ScrollReveal({
 sr.reveal('.home__title',{});
 sr.reveal('.button',{ delay: 200 });
 sr.reveal('.home__img',{ delay: 400 });
+sr.reveal('.home__img-mobile',{ delay: 400 });
 sr.reveal('.home__social-icon',{ interval: 200 });
 
 
 /*SCROLL ABOUT*/
 // sr.reveal('.about__img',{});
 // sr.reveal('.about__subtitle',{ delay: 400 });
-sr.reveal('.about__text',{ delay: 400 });
+sr.reveal('.about__alltext',{ delay: 400 });
 
 /*SCROLL SKILLS*/
 // sr.reveal('.skills__subtitle',{});
-// sr.reveal('.skills__text',{});
+sr.reveal('.skills__text',{});
 // sr.reveal('.skills__data',{ interval: 200 });
 sr.reveal('.skills__img',{ delay: 600 });
 // sr.reveal('.skills__list',{ delay: 600 });
