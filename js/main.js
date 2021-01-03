73fdb0f5df613bfd63eb01da02b2814d47d2f00e
Mike <mi5hqaaaaov@gmail.com>
2021-01-03 22:08:06 +0200

    chang4

--- js/main.js
+++ js/main.js
@@ -46,16 +46,16 @@ trigger.forEach((item,i) => {
 
 
 /*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-});
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '80px',
+//     duration: 2000,
+//     reset: true
+// });
 
 /*SCROLL HOME*/
-sr.reveal('.home__title',{});
-sr.reveal('.button',{ delay: 200 });
+// sr.reveal('.home__title',{});
+// sr.reveal('.button',{ delay: 200 });
 // sr.reveal('.home__img',{ delay: 400 });
 // sr.reveal('.home__social-icon',{ interval: 200 });
 
@@ -63,11 +63,11 @@ sr.reveal('.button',{ delay: 200 });
 /*SCROLL ABOUT*/
 // sr.reveal('.about__img',{});
 // sr.reveal('.about__subtitle',{ delay: 400 });
-sr.reveal('.about__alltext',{ delay: 400 });
+// sr.reveal('.about__alltext',{ delay: 400 });
 
 /*SCROLL SKILLS*/
 // sr.reveal('.skills__subtitle',{});
-sr.reveal('.skills__text',{});
+// sr.reveal('.skills__text',{});
 // sr.reveal('.skills__data',{ interval: 200 });
 // sr.reveal('.skills__img',{ delay: 600 });
 // sr.reveal('.skills__list',{ delay: 600 });
@@ -79,7 +79,7 @@ sr.reveal('.skills__text',{});
 
 /*SCROLL CONTACT*/
 // sr.reveal('.contact__input',{ interval: 200 });
-sr.reveal('.contact2',{ interval: 200 });
+// sr.reveal('.contact2',{ interval: 200 });
 
 
 
